4036996882620e4bb1499d180393ff74c7e7bf3c
0x7o <danil.kononyuk@ya.ru>
2024-06-13 13:39:59 +0800

    Initial changes

--- src/the-data.py
+++ src/the-data.py
@@ -63,7 +63,7 @@ def run():
             ),
             JsonlWriter(f"{FILTERING_OUTPUT_PATH}/output/{DUMP_TO_PROCESS}"),
         ],
-        tasks=8000,
+        tasks=4,
         logging_dir=f"./logs/base_processing/{DUMP_TO_PROCESS}",
         randomize_start_duration=180,  # don't hit the bucket all at once with the list requests
     )
