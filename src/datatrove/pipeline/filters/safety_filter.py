ab2eb2d2142c7d85dfeec05a3cf9d59910b5a6b9
0x7o <danil.kononyuk@ya.ru>
2024-07-07 16:47:10 +0800

    Inference bert

--- src/datatrove/pipeline/filters/safety_filter.py
+++ src/datatrove/pipeline/filters/safety_filter.py
@@ -8,14 +8,10 @@ class SafetyFilter(BaseFilter):
     _requires_dependencies = ["transformers"]
 
     def __init__(self, model_name_or_path: str, exclusion_writer: DiskWriter = None):
-        from transformers import AutoTokenizer, AutoModelForSequenceClassification
+        from transformers import pipeline
 
         super().__init__(exclusion_writer)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.model = AutoModelForSequenceClassification.from_pretrained(
-            model_name_or_path
-        )
-        self.model.eval()
+        self.pipe = pipeline("text-classification", model=model_name_or_path)
         self.bad = "2 3 4 5 6 7 8 9 14".split(" ")
 
     def filter(self, doc: Document) -> bool:
@@ -25,13 +21,8 @@ class SafetyFilter(BaseFilter):
         Returns:
             is_filter
         """
-        inputs = self.tokenizer(
-            doc.text, return_tensors="pt", padding=True, truncation=True, max_length=128
-        )
-        logits = self.model(**inputs).logits
-        predicted_class_id = logits.argmax().item()
-        result = self.model.config.id2label[predicted_class_id]
-        label = result.split("_")[-1]
+        result = self.pipe(doc.text, padding=True, truncation=True, max_length=128)[0]
+        label = result["label"].split("_")[-1]
         if label in self.bad and result["score"] > 0.8:
             return False
         return True
